a30ec989733a58eae14fb7f2295880ef8b90b111
ConorPai <baiguangnan@163.com>
2017-09-10 18:44:23 +0800

    Commit Code

---
+++ js/index.js
@@ -0,0 +1,13 @@
+﻿/**
+ * Created by paiconor on 2016/12/2.
+ */
+$(window).on('load', function () {
+
+    $(".button-collapse").sideNav();
+    $('.slider').slider();
+});
+
+window.onresize = function () {
+    var mainheight = document.body.clientHeight - $(".bodynav").height();
+    $(".bodymain").css({"height": mainheight});
+};
