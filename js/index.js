ac362a896cdcf7842b5ef472a6b5d025044f8332
ConorPai <baiguangnan@163.com>
2017-10-12 16:49:21 +0800

    使用lazyload加快网页显示速度

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
  * Created by paiconor on 2016/12/2.
  */
 $(window).on('load', function () {
+    const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+    observer.observe();
 
     $(".button-collapse").sideNav();
     $('.slider').slider();
